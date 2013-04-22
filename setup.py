5ba3406fde18e24aab5b37d6a5e203dc9c13b077
Jonathan Halcrow <jonathan.halcrow@gmail.com>
2013-04-22 19:52:47 -0400

    Setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Our New Ball and Chain RSVP',
+    version='0.1',
+    description='RSVP',
+    author='Jonathan Halcrow',
+    url='http://www.github.com/jhalcrow/ournewballandchain.git',
+    packages=['ournewballandchain']
+)
