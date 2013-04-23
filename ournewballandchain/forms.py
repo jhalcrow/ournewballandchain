96237a4e17f9fa50289a9cc74ddd3f6e9b883e35
Jonathan Halcrow <jonathan.halcrow@gmail.com>
2013-04-22 23:09:13 -0400

    Fix RSVP for non-attending

--- ournewballandchain/forms.py
+++ ournewballandchain/forms.py
@@ -1,21 +1,22 @@
-from flask.ext.wtf import Form, TextField, Required, BooleanField, IntegerField, TextAreaField, ValidationError
+from flask.ext.wtf import Form, TextField, Required, BooleanField, IntegerField, TextAreaField, ValidationError, Optional
 from flask.ext.wtf.html5 import EmailField, IntegerRangeField
 
 class RSVPForm(Form):
     email = EmailField("Email (if you want to get updates from us)")
-    attending = BooleanField("Attending")
-    guests = IntegerField("Number of Guests (including yourself)")
+    attending = BooleanField("Attending", default=True, validators=[Optional()])
+    guests = IntegerField("Number of Guests (including yourself)", default=1, validators=[Optional()])
     guest_names = TextField("Names of your guests")
     note = TextAreaField("Anything you'd like to add")
 
     def validate_guests(form, field):
-        if field.data >= 10:
-            raise ValidationError(
-                '''Sorry, that's too many people.
-                Contact Kate or Jonathan if you really want to bring %s guests'''
-                 % field.data)
-        if form.attending.data and field.data <= 0:
-            raise ValidationError('Please enter the number of people attending including yourself.')
+        if form.attending.data:
+            if field.data >= 10:
+                raise ValidationError(
+                    '''Sorry, that's too many people.
+                    Contact Kate or Jonathan if you really want to bring %s guests'''
+                     % field.data)
+            if not field.data or field.data <= 0:
+                raise ValidationError('Please enter the number of people attending including yourself.')
 
 
 class RSVPFreeForm(RSVPForm):
